19cb74452a6d33eae8dbce5f6d88a00313c31198
GitHub Action <action@github.com>
2025-11-08 12:43:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/wl2027/remotedebugplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/wl2027/remotedebugplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.wl2027.remotedebugplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
